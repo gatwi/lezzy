5553445fdb29f56733efa433874726d5a9b1d054
gatwi <faith.murithi@student.moringaschool.com>
2022-11-16 15:12:03 +0300

    Creating and styling the hero img component.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import HeroImage from '../components/HeroImage';
 import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HeroImage />
     </div>
   )
 }
