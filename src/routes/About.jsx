cd273ded409c7c977d34a4b3fc7076394274d5ca
gatwi <faith.murithi@student.moringaschool.com>
2022-11-26 15:54:36 +0300

    First draft for the About Us page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Hero2 from '../components/Hero2';
+import AboutContent from '../components/AboutContent';
 
 const About = () => {
   return (
     <div>
       <Navbar />
       <Hero2 heading="Who We Are." text="An Afropolitan Queer Feminists Group"/>
+      <AboutContent />
       <Footer />
     </div>
   )
