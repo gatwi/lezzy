ec99bacb34a39b45346f3a9088d92d844e37ac22
gatwi <faith.murithi@student.moringaschool.com>
2022-11-26 12:47:16 +0300

    Basic styling of the catalog component

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -6,16 +6,17 @@ import { NavLink } from 'react-router-dom';
 const Events = () => {
   return (
     <div className='events-container'>
-        <h1 className='catalog-heading'>Catalog</h1>
+        <h1 className='catalog-heading'>Events</h1>
         <div className='catalog-container'>
             <div className='catalog-card'>
-                <img src={cat1} alt="image"/>
-                <h2 className='catalog-title'>Catalog Title</h2>
+                <img src={cat1} alt=""/>
+                <h2 className='catalog-title'>Games and Trivia</h2>
                 <div className='cat-details'>
                     <p>Awesome Lorem texts</p>
                 </div>
                 <div className='cat-btns'>
                     <NavLink to="url.com" className="btn">View</NavLink>
+                    <NavLink to="url.com" className="btn">Source</NavLink>
                 </div>
             </div>
         </div>
