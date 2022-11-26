07d1a4be5059b9b40a77267573fe8ec81cfaacd7
gatwi <faith.murithi@student.moringaschool.com>
2022-11-26 14:00:38 +0300

    Dynamically mapping props with a local API

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import "./EventsStyle.css";
-import cat1 from "../assets/cat1.jpeg";
-import { NavLink } from 'react-router-dom';
+import EventCard from './EventCard';
+import EventCardData from './EventsData';
+
 
 const Events = () => {
   return (
     <div className='events-container'>
         <h1 className='catalog-heading'>Events</h1>
         <div className='catalog-container'>
-            <div className='catalog-card'>
-                <img src={cat1} alt=""/>
-                <h2 className='catalog-title'>Games and Trivia</h2>
-                <div className='cat-details'>
-                    <p>Awesome Lorem texts</p>
-                </div>
-                <div className='cat-btns'>
-                    <NavLink to="url.com" className="btn">View</NavLink>
-                    <NavLink to="url.com" className="btn">Source</NavLink>
-                </div>
-            </div>
+           {EventCardData.map((val, ind) =>{
+            return(
+                <EventCard
+                    key={ind}
+                    imgsrc={val.imgsrc}
+                    title={val.title}
+                    text={val.text}
+                    view={val.view}
+                />
+            )
+           })}
         </div>
     </div>
-  )
+  );
 }
 
 export default Events;
---
+++ src/components/EventsData.jsx
@@ -0,0 +1,26 @@
+import cat1 from "../assets/cat1.jpeg";
+import cat2 from "../assets/cat2.jpeg";
+import cat3 from "../assets/cat3.jpeg"
+
+const EventCardData = [
+    {
+        imgsrc: cat1,
+        title: "Games and Trivia",
+        text: "This is a safe and grown project collection of activities aimed at facilitating discussions on LGBTQ+ identities, allyship, gender and sexuality, and effective awareness on the lives and times of Queers",
+        view: "https://www.instagram.com/26and_above/",
+    },
+    {
+        imgsrc: cat2,
+        title: "Twende Cinema",
+        text: "This is a safe and grown project collection of activities aimed at facilitating discussions on LGBTQ+ identities, allyship, gender and sexuality, and effective awareness on the lives and times of Queers",
+        view: "https://www.instagram.com/26and_above/",
+    },
+    {
+        imgsrc: cat3,
+        title: "Oontz and Good Vibes",
+        text: "This is a safe and grown project collection of activities aimed at facilitating discussions on LGBTQ+ identities, allyship, gender and sexuality, and effective awareness on the lives and times of Queers",
+        view: "https://www.instagram.com/26and_above/",
+    }
+];
+
+export default EventCardData;
